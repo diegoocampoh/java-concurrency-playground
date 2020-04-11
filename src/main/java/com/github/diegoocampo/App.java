6bc12449f689fe0e546fdad3baf27510b20a6b3f
Diego Ocampo <diegoocampoh@gmail.com>
2020-04-11 19:15:18 +1000

    Initial commit

---
+++ src/main/java/com/github/diegoocampo/App.java
@@ -0,0 +1,13 @@
+package com.github.diegoocampo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
